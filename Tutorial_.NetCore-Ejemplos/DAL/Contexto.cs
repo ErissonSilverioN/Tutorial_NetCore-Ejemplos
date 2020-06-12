d4dd8cdde87040452d0a98a38ea85195a4990ece
Erisson Silverio Nu�ez <erisson_silverio@ucne.edu.do>
2020-06-12 18:21:38 -0400

    DBContext Creado

---
+++ Tutorial_.NetCore-Ejemplos/DAL/Contexto.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tutorial_.NetCore_Ejemplos.Models;
+
+namespace Tutorial_.NetCore_Ejemplos.DAL
+{
+    class Contexto : DbContext
+    {
+        public DbSet<Student> students  { get; set; }
+        public DbSet<Course> courses { get; set; }
+        public DbSet<Teacher> teachers { get; set; }
+        public DbSet<StudentAddress> studentAddresses { get; set; }
+        public DbSet<StudentCourse> studentCourses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server = .\SqlExpress; Database = SchoolDB; Trusted_Connection = True; ");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
